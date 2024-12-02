3ea0490d6869d8747c7984ae754d943fb9407555
Adrien <adrienribeiro999@gmail.com>
2024-12-02 23:07:30 +0100

    1save

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ console.log(personne)
 
 personne.afficherSolde();
 console.log(personne)
+
+//------------------------------------------------
+
+fetch("https://jsonplaceholder.typicode.com/todos", {
+       method: 'GET',
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/todos", {
+     method: 'POST',
+    body: JSON.stringify({
+        title: '',
+        body: 'bar',
+        userId: 1,
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+    
